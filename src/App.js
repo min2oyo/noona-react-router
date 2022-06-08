7648d5f9391bb7ab8f0ac604c238760d84fb9d07
min2oyo <min2u@outlook.com>
2022-06-08 18:48:19 +0900

    Redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from 'react';
 import AboutPage from './page/AboutPage';
 import Homepage from './page/Homepage';
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
+import LoginPage from './page/LoginPage';
+import UserPage from './page/UserPage';
 
 function App() {
+
+  const [authenticate, setAuthenticate] = useState(false);
+
+  const PrivateRoute = () => {
+    return authenticate ? <UserPage /> : <Navigate to='/login' />;
+  };
+
   return (
     <div>
       <Routes>
@@ -13,6 +23,8 @@ function App() {
         <Route path='/about' element={<AboutPage />} />
         <Route path='/products' element={<ProductPage />} />
         <Route path='/products/:id/:num' element={<ProductDetailPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/user' element={<PrivateRoute />} />
       </Routes>
     </div>
   );
