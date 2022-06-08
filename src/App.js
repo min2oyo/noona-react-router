43500111057face662c9399ccad456381fd553d6
min2oyo <min2u@outlook.com>
2022-06-08 18:20:06 +0900

    Restful Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import AboutPage from './page/AboutPage';
 import Homepage from './page/Homepage';
-import { Routes, Route } from "react-router-dom";
+import ProductPage from './page/ProductPage';
+import ProductDetailPage from './page/ProductDetailPage';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/about' element={<AboutPage />} />
+        <Route path='/products' element={<ProductPage />} />
+        <Route path='/products/:id' element={<ProductDetailPage />} />
       </Routes>
     </div>
   );
