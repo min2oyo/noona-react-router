26cf8169ceb41caafa05a1b90d7f7c2dee945b0e
min2oyo <min2u@outlook.com>
2022-06-08 18:26:22 +0900

    useParams

--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
+
+  const { id } = useParams();
+
   return (
     <div>
-      <h1>ProductDetailPage</h1>
+      <h1>ProductDetailPage {id}</h1>
     </div>
   );
 };
