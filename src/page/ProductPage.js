8e269ca7d45d1960e54f676f0adcbaf5de044a60
min2oyo <min2u@outlook.com>
2022-06-08 18:35:57 +0900

    useSearchParams

--- src/page/ProductPage.js
+++ src/page/ProductPage.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useSearchParams } from "react-router-dom";
 
 const ProductPage = () => {
+
+  let [query, setQuery] = useSearchParams();
+  console.log(`query:`, query.get(`q`));
+
   return (
     <div>
       <h1>ProductPage</h1>
